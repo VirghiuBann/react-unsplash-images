d5dc86f0dd3fa57373dbc851c763234f5b0c675d
virghiu <virghiu@gmail.com>
2024-03-25 14:18:57 -0400

    add handle submit

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,8 +1,13 @@
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const searchValue = e.target.elements.search.value
+    if (!searchValue) return
+  }
   return (
     <section>
       <h1 className='title'>unsplash images</h1>
-      <form className='search-form'>
+      <form className='search-form' onSubmit={handleSubmit}>
         <input
           type='text'
           className='form-input search-input'
